228c31199f947765c5842e5167501eacf58dc020
YamOhana <yamoh1993@gmail.com>
2023-07-31 10:41:30 +0300

    queue

--- controllers/adHocController.js
+++ controllers/adHocController.js
@@ -1,17 +1,17 @@
 const CachedObject = require('../models/cachedObject');
 const logger = require('../logger');
 const simulateTimeConsumingTask = require('../utils');
+const queue = require('./queue/queue');
+
+
 
 exports.adHocCacheRefresh = async (objectId) => {
   try {
     logger.info(`Ad-hoc cache refresh request received for object ID: ${objectId}`);
-
     logger.info(`Starting cache refresh for object ID: ${objectId}`);
-
+    await queue.sendCacheRefreshMessage(objectId);
     await simulateTimeConsumingTask();
-
     logger.info(`Cache refresh completed for object ID: ${objectId}`);
-
     const cachedObject = await CachedObject.findOneAndUpdate(
       { ID: objectId },
       {
@@ -26,7 +26,6 @@ exports.adHocCacheRefresh = async (objectId) => {
     );
 
     logger.info(`Cached object updated in the database: ${cachedObject}`,);
-
     return cachedObject;
   } catch (err) {
     logger.error(`Error in adHocCacheRefresh: ${err}`,);
--- controllers/batchController.js
+++ controllers/batchController.js
@@ -1,37 +1,30 @@
 const CachedObject = require('../models/cachedObject');
 const logger = require('../logger');
-const simulateTimeConsumingTask = require('../utils');
+const queue = require('./queue/queue');
 
 exports.batchCacheRefresh = async (req, res) => {
   try {
     const { criteria } = req.body;
-
-    logger.info('Batch cache refresh request received with criteria: ${criteria}');
+    logger.info('Batch cache refresh request received with criteria:', criteria);
     await simulateTimeConsumingTask();
-
-
     const cachedObjectsToUpdate = await CachedObject.find(criteria);
 
     for (const cachedObject of cachedObjectsToUpdate) {
-      logger.info(`Starting cache refresh for object ID: ${cachedObject.ID}`);
-
-      await simulateTimeConsumingTask();
-
-      logger.info(`Cache refresh completed for object ID: ${cachedObject.ID}`);
-
-      cachedObject.lastUpdateDate = new Date();
-      cachedObject.cachedData = 'Sample updated cached data';
-      cachedObject.requestType = 'batch';
-
-      await cachedObject.save();
+      logger.info('Starting cache refresh for object ID:', cachedObject.ID);
+      await queue.sendMessageToQueue(cachedObject.ID);
+      logger.info('Cache refresh request sent for object ID:', cachedObject.ID);
     }
-
     logger.info('Batch cache refresh completed.');
-
     res.json({ message: 'Batch cache refresh completed successfully.' });
   } catch (err) {
-    logger.error(`Error in batchCacheRefresh: ${err}`);
-
+    logger.error('Error in batchCacheRefresh:', err);
     res.status(500).json({ error: 'An error occurred during batch cache refresh.' });
   }
+};
+
+const simulateTimeConsumingTask = () => {
+  return new Promise((resolve) => {
+    logger.info('Simulating time-consuming batch cache refresh task...');
+    setTimeout(resolve, 3000);
+  });
 };
